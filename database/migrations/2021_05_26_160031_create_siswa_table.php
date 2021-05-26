54dd62c099c606733c5adacff82518979d16a84c
agung-permana <agungpermana490@gmail.com>
2021-05-26 23:08:26 +0700

    first commit

---
+++ database/migrations/2021_05_26_160031_create_siswa_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('kelas_id');
+            $table->string('nis');
+            $table->string('nama');
+            $table->string('no_telp');
+            $table->text('alamat');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,22 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="index.html">E-Voting</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">EV</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Dashboard</li>
+        <li class="active">
+        <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        </li>
+
+        <li class="">
+            <a href="#" class="nav-link"><i class="fas fa-users"></i><span>Siswa</span></a>
+        </li>
+
+        <li class="">
+            <a href="#" class="nav-link"><i class="fas fa-user"></i><span>Kandidat</span></a>
+        </li>
+    </ul>
+</aside>
